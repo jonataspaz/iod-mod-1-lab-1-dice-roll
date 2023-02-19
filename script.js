722bc646e5193b32d180e29d00fdae632c71cd84
Jonatas Paz <jopaz9@gmail.com>
2023-02-20 00:08:24 +1300

    Update script.js

--- script.js
+++ script.js
@@ -7,18 +7,31 @@ let images = [
     "images/d6_dice/d6_dice_6_430.png"
 ];
 let currentIndex = 0;
-let 
-// let intervalId = setInterval(changeImage, 200);
+let diceCounter = 0;
+
+
 
 function startChange() {
+  if (diceCounter = 5) {
     setInterval(changeImage, 300);
-}
+    setInterval(countTime, 1000);
+  }
+  else {
+    currentIndex = 6;
+  }
+
+};
 
 function changeImage() {
   let image = document.getElementById("dice_d6");
   currentIndex = (currentIndex + 1) % images.length;
   image.src = images[currentIndex];
 
-}
+};
 
-console.log(Math.floor(Math.random() * 7));
+function countTime() {
+  diceCounter++;
+  console.log(diceCounter)
+};
+
+// console.log(Math.floor(Math.random() * 7));
