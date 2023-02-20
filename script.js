afdba84fcb09ca7d8ac6dd9ffaf1744ac6f01514
Jonatas Paz <jopaz9@gmail.com>
2023-02-20 17:58:34 +1300

    Finish function to change dice and random number

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let images = [
+
+function mainFunction(){
+
+  let images = [
     "images/d6_dice/d6_dice_1_430.png",
     "images/d6_dice/d6_dice_2_430.png",
     "images/d6_dice/d6_dice_3_430.png",
@@ -8,30 +11,19 @@ let images = [
 ];
 let currentIndex = 0;
 let diceCounter = 0;
+let intervalId = setInterval(changeImage, 100);
 
-
-
-function startChange() {
-  if (diceCounter = 5) {
-    setInterval(changeImage, 300);
-    setInterval(countTime, 1000);
-  }
-  else {
-    currentIndex = 6;
-  }
-
-};
 
 function changeImage() {
   let image = document.getElementById("dice_d6");
   currentIndex = (currentIndex + 1) % images.length;
   image.src = images[currentIndex];
-
-};
-
-function countTime() {
   diceCounter++;
-  console.log(diceCounter)
-};
+  console.log(diceCounter);
+  if (diceCounter == 16) {
+    clearInterval(intervalId);
+    image.src = images[Math.floor(Math.random() * 7)]
+  }
 
-// console.log(Math.floor(Math.random() * 7));
+};
+};
